6cd741256fab4d71ce89870817f409137cbc09e4
arielmusa <arielmusainfo@gmail.com>
2025-05-14 16:44:35 +0200

    postsController.js: add error status for all routes.
    add tag filter for index route

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,12 +1,31 @@
 import { posts } from "../data/posts.js";
 
 const index = (req, res) => {
-  res.json(posts);
+  const queryTag = req.query.tags;
+
+  let filteredPosts = posts;
+
+  if (queryTag) {
+    filteredPosts = filteredPosts.filter((post) =>
+      post.tags.includes(queryTag)
+    );
+  }
+
+  if (filteredPosts.length === 0) {
+    return res.status(404).json({ message: "Item not found" });
+  }
+  res.json(filteredPosts);
 };
 
 const show = (req, res) => {
   const id = parseInt(req.params.id);
   const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({
+      error: "Not found",
+      message: "Item not found",
+    });
+  }
   res.json(post);
 };
 
@@ -16,18 +35,39 @@ const store = (req, res) => {
 
 const update = (req, res) => {
   const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({
+      error: "Not found",
+      message: "Item not found",
+    });
+  }
   res.send(`Aggiornamento dati post id: ${id}`);
 };
 
 const modify = (req, res) => {
   const { id } = req.params;
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({
+      error: "Not found",
+      message: "Item not found",
+    });
+  }
   res.send(`Modifica post id: ${id}`);
 };
 
 const destroy = (req, res) => {
   const id = parseInt(req.params.id);
   console.log(`Eliminazione post id: ${id}`);
-  res.status(204);
-  posts.splice(posts.indexOf(posts.find((post) => post.id === id)), 1);
+  const post = posts.find((post) => post.id === id);
+  if (!post) {
+    return res.status(404).json({
+      error: "Not found",
+      message: "Item not found",
+    });
+  }
+  posts.splice(posts.indexOf(post), 1);
+  res.sendStatus(204);
 };
 export { index, show, store, update, modify, destroy };
