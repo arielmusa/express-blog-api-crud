360928deab0c0aa146a74f23ba68a5ae6505cc64
arielmusa <arielmusainfo@gmail.com>
2025-05-16 17:42:44 +0200

    middleware: add handleError.js to handle status 500 errors.
    add notFound.js to handle wrong call to endpoints

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import { router } from "./routers/router.js";
+import { handleError } from "./middlewares/handleError.js";
+import { notFound } from "./middlewares/notFound.js";
 
 const app = express();
 const { APP_HOST, APP_PORT } = process.env;
@@ -10,6 +12,9 @@ const url = `${APP_HOST}${APP_PORT ? ":" + APP_PORT : ""}`;
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/posts", router);
+
+app.use(handleError);
+app.use(notFound);
 
 app.listen(APP_PORT, () => {
   console.log(`Listening ${url}`);
